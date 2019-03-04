2b8290141d79353eed5eb4ba08aa767b96f0be96
KaroonaB <Karoonabhagat1999@gmail.com>
2019-03-04 13:46:39 -0500

    commiting

---
+++ src/midtermexam_versiona_extensioncode/TestUserprofile.java
@@ -0,0 +1,20 @@
+package midtermexam_versiona_extensioncode;
+import java.util.Scanner;
+/**
+ * @author Rich Smith at ZenOfProgramming.com
+ */
+public class TestUserprofile
+{
+   public static void main (String[] args)
+   {
+      Scanner name = new Scanner(System.in);
+      System.out.println("Enter your name:");
+      String userID = name.nextLine();
+
+      Scanner gen = new Scanner(System.in);
+      System.out.println("Enter your genre:");
+      String genre = gen.nextLine();
+
+   }
+
+}
